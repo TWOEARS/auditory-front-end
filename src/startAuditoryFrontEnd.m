6bdc1588365d77a2475d098ce1b98bfa55471d95
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-10-08 14:37:42 +0200

    renaming and new start up behavior

--- src/startAuditoryFrontEnd.m
+++ src/startAuditoryFrontEnd.m
@@ -2,6 +2,7 @@
 % extraction framework and set up the needed pathes. Make sure to clear
 % yourself the Matlab workspace, if that is necessary.
 
-path = fileparts(mfilename('fullpath'));
-addpath(genpath([path filesep 'src'),genpath([path filesep 'test']))
-clear path
+basePath = [fileparts(mfilename('fullpath')) filesep];
+addpath(genpath(basePath))
+
+clear basePath
