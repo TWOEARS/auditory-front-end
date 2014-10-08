24ed7ba53b67def8932c424bcb587fecbd1169d1
Remi Decorsiere <rd@elektro.dtu.dk>
2014-10-08 14:45:17 +0200

    Investigating cascading Filters

--- src/Filters/bwFilter.m
+++ src/Filters/bwFilter.m
@@ -1,9 +1,9 @@
 classdef bwFilter < filterObj
     
     methods
-        function obj = bwFilter(fs,order,cutOffHz,struct)
+        function obj = bwFilter(fs,order,cutOffHz,struct,cascade)
             if nargin > 0
-                if nargin < 4; struct = 'Direct-Form II Transposed'; end
+                if nargin < 4||isempty(struct); struct = 'Direct-Form II Transposed'; end
                 if nargin < 3; cutOffHz = 1000; end % Dau1996 model
                 if nargin < 2; order = 2; end % Dau1996
                 
