569c573db64f52cc9190b6bf522efd30c9c09f22
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-10-08 12:07:11 +0200

    Corrected start function

--- startAuditoryFrontEnd.m
+++ startAuditoryFrontEnd.m
@@ -1,7 +1,7 @@
-% This script adds the necessary paths for the Two!Ears Auditory Cues
-% extraction framework and set up the needed pathes. Make sure to
-% clear yourself the Matlab workspace, if that is necessary.
+% This script adds the necessary paths for the Two!Ears Auditory Front-End
+% extraction framework and set up the needed pathes. Make sure to clear
+% yourself the Matlab workspace, if that is necessary.
 
 path = fileparts(mfilename('fullpath'));
-addpath(genpath(path),genpath([path filesep '..' filesep 'test']))
+addpath(genpath([path filesep 'src'),genpath([path filesep 'test']))
 clear path
