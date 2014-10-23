5ba2d8f39e7073a9c5fe60410e4001666209b068
Remi Decorsiere <rd@elektro.dtu.dk>
2014-10-23 17:04:30 +0200

    Added plotting routines for spectral features

--- test/Development_test_scripts/test_startup.m
+++ test/Development_test_scripts/test_startup.m
@@ -1,6 +1,4 @@
-% Do nothing (for the standalone release version)
-
-% % Start Two!Ears Auditory Front-End framework
-% startTwoEars('../../AuditoryFrontEnd.xml');
-% % Add folder with test signals
-% addpath('../Test_signals');
+% Start Two!Ears Auditory Front-End framework
+startTwoEars('../../AuditoryFrontEnd.xml');
+% Add folder with test signals
+addpath('../Test_signals');
