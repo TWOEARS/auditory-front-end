1b8dbbb7a47e1dfbe46b46e646fa07e21f738d71
kim.chungeun.ryan <c.k.kim@tue.nl>
2015-11-30 15:57:54 +0100

    MOC and precedence effect demo scripts cleaned up

--- test/DEMO_MOC.m
+++ test/DEMO_MOC.m
@@ -3,91 +3,85 @@ close all
 clc
 
 %% CREATE TEST SIGNALS
-% sinusoid with some onset/offset ramps
-% adopted from MAP1_14h test codes to potentially test against the MAP
-% implementation
-fsHz = 44100;             
-toneFrequency= [520 3980];          % (Hz)
-toneDuration = 0.05;                % 
-beginSilence=0.05;
-endSilence=0;
-rampDuration=.0025;                 % raised cosine ramp (seconds)
-leveldBSPL= 0:10:90;   
 
-% calibration factor (see Jepsen et al. 2008)
-dBSPLCal = 100;         % signal amplitude 1 should correspond to max SPL 100 dB
+% sinusoid with onset/offset ramps
+% The scripts for the input signals were adopted from MAP1_14h codes
+% (Matlab Auditory Periphery, Hearing Research Lab, University of Essex) 
+% to potentially test against the MAP implementation
+% one or more input frequencies and levels can be tested 
+fsHz = 44100;                       % Sampling frequency
+toneFrequency= [520 3980];          % Input tone frequency (Hz)
+toneDuration = 0.05;                % Duration of the tone (s)
+initialSilence = 0.05;              % Initial silence duration (s)
+endSilence = 0;                     % Silence duration after the tone part (s)
+rampDuration =.0025;                % raised cosine ramp duration (s)
+leveldBSPL = 0:10:90;               % Input tone level to be tested (dB SPL)
+
+% AFE online processing chunk size in seconds
+chunkSizeSec = 10E-3;
+
+% Calibration factor (see Jepsen et al. 2008)
+dBSPLCal = 100;         % signal amplitude 1 should correspond to 100 dB
 ampCal = 1;             % signal amplitude to correspond to dBSPLRef
 pRef = 2e-5;            % reference sound pressure (p0)
 pCal = pRef*10^(dBSPLCal/20);
 calibrationFactor = ampCal*10.^((leveldBSPL-dBSPLCal)/20);
-levelPressure = pRef*10.^(leveldBSPL/20);
 
-% define 20-ms onset sample after ramp completed 
-%  allowing 5-ms response delay
-onsetPTR1=round((rampDuration+ beginSilence +0.005)*fsHz);
-onsetPTR2=round((rampDuration+ beginSilence +0.005 + 0.020)*fsHz);
-% last half 
-lastHalfPTR1=round((beginSilence+toneDuration/2)*fsHz);
-lastHalfPTR2=round((beginSilence+toneDuration-rampDuration)*fsHz);
-% output window
-outputWindowStart = round((beginSilence)*fsHz);
-outputWindowEnd = round((beginSilence+toneDuration)*fsHz);
+% Window position indices in samples over which the output will be calculated
+outputWindowStart = round((initialSilence)*fsHz);
+outputWindowEnd = round((initialSilence+toneDuration)*fsHz);
 
-dt = 1/fsHz; % seconds
-toneTime = dt: dt: toneDuration;
-totalTime = dt:dt:beginSilence+toneDuration+endSilence;
+% Time indexing
+dt = 1/fsHz;                        % seconds
+toneTime = dt:dt:toneDuration;      % tone time index
+totalTime = dt:dt:initialSilence+toneDuration+endSilence;   
+                                    % time index for the whole input signal
 
-% calculate ramp factor
-if rampDuration>0.5*toneDuration, rampDuration=toneDuration/2; end
-rampTime=dt:dt:rampDuration;
-ramp=[0.5*(1+cos(2*pi*rampTime/(2*rampDuration)+pi)) ...
+% Calculate ramp factor to be multiplied to the input signals
+if rampDuration>0.5*toneDuration, rampDuration = toneDuration/2; end
+rampTime = dt:dt:rampDuration;
+ramp = [0.5*(1+cos(2*pi*rampTime/(2*rampDuration)+pi)) ...
     ones(1,length(toneTime)-length(rampTime))];
-ramp_temp = repmat(ramp, [length(leveldBSPL), 1]);  % to be multiplied to inputSignal
+ramp_temp = repmat(ramp, [length(leveldBSPL), 1]);  
 
-% derive silence parts
-intialSilence= zeros(1,round(beginSilence/dt));
-finalSilence= zeros(1,round(endSilence/dt));
+% derive silence parts to be attached to the tone
+intialSilence = zeros(1,round(initialSilence/dt));
+finalSilence = zeros(1,round(endSilence/dt));
 
-% Online processing parameters
-% Chunk size in samples (for online processing)
-chunkSize = fsHz * 10E-3;    
-
-% Number of chunks in the signal - use inputSignal to calculate the signal
-% length
+% Adjusting the input length for online/offline processing comparison,
+% depending on the specified chunk size
+% AFE online processing chunk size in samples 
+chunkSize = fsHz*chunkSizeSec;    
+% Number of chunks in the whole signal duration
 n_chunks = ceil(length(totalTime)/chunkSize);
-
 % Zero-pad the signal for online vs. offline direct comparison
 finalSilence = [finalSilence zeros(1, n_chunks*chunkSize-length(totalTime))];
 % Now the lengths of finalSilence section and totalTime have changed
 totalTime = (1:length(toneTime)+length(intialSilence)+length(finalSilence))/fsHz;
 
+% Prepare the storage for the input signals (with varying level and tone
+% frequency)
 inputSignalMatrix = zeros(length(leveldBSPL), ...
     length(totalTime), ...
     length(toneFrequency));
 
-for ii=1:length(toneFrequency)
-    inputSignal=sin(2*pi*toneFrequency(ii)'*toneTime) .* sqrt(2);      % amplitude -1~+1 -> sqrt(2)
-    % "input amplitude of 1 corresponds to a maximum SPL of 100 dB"
-    % --> RMS OF 1 NOW CORRESPONDS TO 100 dB SPL! 
-    % calibration: calculate difference between input level dB SPL and the
-    % given SPL for calibration (100 dB)
+% Generate input signals and store in the input signal matrix
+for ii = 1:length(toneFrequency)
+    % amplitude -1~+1 -> sqrt(2)
+    inputSignal = sin(2*pi*toneFrequency(ii)'*toneTime) .* sqrt(2);      
+            % --> RMS OF 1 NOW CORRESPONDS TO 100 dB SPL 
+    
     inputSignal = calibrationFactor'*inputSignal;
-    % "signal amplitude is scaled in pascals in prior to OME"
+            % "signal amplitude is scaled in pascals in prior to OME"
     
     % apply ramp
-    inputSignal=inputSignal.*ramp_temp;
-    ramp_temp=fliplr(ramp_temp);
-    inputSignal=inputSignal.*ramp_temp;
-    % add silence
-    inputSignal= [repmat(intialSilence, [length(leveldBSPL), 1]) ...
+    inputSignal = inputSignal.*ramp_temp;
+    ramp_temp = fliplr(ramp_temp);
+    inputSignal = inputSignal.*ramp_temp;
+    % add initial/final silence
+    inputSignal = [repmat(intialSilence, [length(leveldBSPL), 1]) ...
         inputSignal repmat(finalSilence, [length(leveldBSPL), 1])]; 
-    
-    % Obtain the dboffset currently used
-    dboffset=dbspl(1);
-
-    % Switch signal to the correct scaling
-    inputSignal=gaindb(inputSignal, dboffset-100);
-    
+    % store into the input signal matrix
     inputSignalMatrix(:, :, ii) = inputSignal;
 end
 
@@ -97,9 +91,6 @@ end
 % matrix to store output (I/O function)
 ioFunctionMatrix = zeros(length(toneFrequency), length(leveldBSPL));
 ioFunctionMatrix_moc = zeros(length(toneFrequency), length(leveldBSPL));
-
-% (AN firing rate-level function)
-rateLevelFunctionMatrix = zeros(length(toneFrequency), length(leveldBSPL));
 
 % (MOC activity-level function)
 mocLevelFunctionMatrix = zeros(length(toneFrequency), length(leveldBSPL));
@@ -129,6 +120,7 @@ nFrames = floor((length(totalTime)-(rm_wSizeSec*fsHz - round(rm_hSizeSec*fsHz)))
 % Introduce outer ear filter (imported from AMT, in src/Tools folder)
 oe_fir = headphonefilter(fsHz);
 
+% Perform processing and save output
 for jj=1:length(toneFrequency)
 
     % Filter input through outer ear filter
@@ -170,36 +162,19 @@ for jj=1:length(toneFrequency)
         
         % DRNL output
         bmOut = dObj.filterbank{1}.Data(:);
-        bmOutMax = max(bmOut);
-        bmOutMaxdB = 20*log10(bmOutMax);
         bmOutRMSdB = 20*log10(rms(bmOut(outputWindowStart:outputWindowEnd)));
-
-        % ratemap output
-        anOut = dObj.ratemap{1}.Data(:);
-        anOutMax = max(anOut);
-        anOutMaxdB = 20*log10(anOutMax);
 
         % DRNL output, with MOC working
         bmOut_moc = dObj_moc.filterbank{1}.Data(:);
-        bmOutMax_moc = max(bmOut_moc);
-        bmOutMaxdB_moc = 20*log10(bmOutMax_moc);
         bmOutRMSdB_moc = 20*log10(rms(bmOut_moc(outputWindowStart:outputWindowEnd)));
-
-        % ratemap maximum output, with MOC working
-        anOut_moc = dObj_moc.ratemap{1}.Data(:);
-        anOutMax_moc = max(anOut_moc);
-        anOutMaxdB_moc = 20*log10(anOutMax_moc);
 
         % MOC maximum output
         mocOut = dObj_moc.moc{1}.Data(:);
         mocOutMax = max(mocOut);
 
-        % Input vs DRNL maximum output
+        % Input vs DRNL RMS output
         ioFunctionMatrix(jj, kk) = bmOutRMSdB;
         ioFunctionMatrix_moc(jj, kk) = bmOutRMSdB_moc;
-
-        % Input vs ratemap maximum output
-        rateLevelFunctionMatrix(jj, kk) = anOutMaxdB;
 
         % Input vs MOC attenuation output
         mocLevelFunctionMatrix(jj, kk) = mocOutMax;
@@ -217,11 +192,6 @@ for jj=1:length(toneFrequency)
             xlabel('Time (s)');
             ylabel('Amplitude (x 1E$^{-3}$)');
             title('DRNL output with reflexive MOC feedback');
-%             figure; plot(bmOut);
-%             figure; plot(bmOut_moc);
-%             figure; plot(anOut);
-%             figure; plot(anOut_moc);
-%             figure; plot(mocOut);
         end
 
     end
@@ -230,6 +200,7 @@ end
 
 %% PLOT RESULTS
 
+% DRNL filterbank input-output characteristics, without MOC processor
 figure;
 set(gcf,'DefaultAxesColorOrder',[0 0 0], ...
     'DefaultAxesLineStyleOrder','-o|--s|:x|-.*');
@@ -241,7 +212,7 @@ title(sprintf('Input-output characteristics of  DRNL filterbank\non-frequency st
 legendCell=cellstr(num2str(toneFrequency', '%-dHz'));
 legend(legendCell, 'Location', 'NorthWest'); legend('boxoff');
 
-
+% DRNL filterbank input-output characteristics, with MOC processor
 figure;
 set(gcf,'DefaultAxesColorOrder',[0 0 0], ...
     'DefaultAxesLineStyleOrder','-o|--s|:x|-.*');
@@ -253,8 +224,10 @@ title(sprintf('Input-output characteristics of  DRNL filterbank with MOC feedbac
 legendCell=cellstr(num2str(toneFrequency', '%-dHz'));
 legend(legendCell, 'Location', 'NorthWest'); legend('boxoff');
 
-
-libermanMOCdata_520Hz = [0 0 0 5 18.5 29.5 36 38 39 37];       % scaled to MOC attenuation in Clark et al. 2012!!
+% Plotting input tone level - MOC attenuation output characteristics, 
+% compared against the data of Liberman 1988
+% Liberman data, NOTE: scaled to MOC attenuation in Clark et al. 2012!!
+libermanMOCdata_520Hz = [0 0 0 5 18.5 29.5 36 38 39 37];       
 libermanMOCdata_3980Hz = [0 0 0 10 17.5 24 30 34 37 38];
 
 figure;
--- test/DEMO_Precedence.m
+++ test/DEMO_Precedence.m
@@ -2,20 +2,68 @@ clear;
 close all
 clc
 
-
 %% LOAD SIGNAL
 
-fsHz=48000;             % Sampling Frequency [Hz]
-ISI=[3];                % Inter-stimulus Interval [ms]
-Ratio=[0];              % lead/lag level ratio [dB]
-StimDuration=200;       % stimulus duration [ms]
+% Input signal, created for the demo using stimulusBraasch function
+% (see below for the input parameters and usage)
 
-% create test stimulus (binaural)
-% usage: STIMULUS(Fs,mode,len,f,bw,itd1,itd2,isi,at,dc)
-% this function is copied from Braasch's model into AFE src/Tools
-x = stimulusBraasch(fsHz, 2, 400, 500, 800, 0.6, -0.6, 3, 20, 20, 0,db2amp(0));
+fsHz = 48000;           % Sampling Frequency [Hz]
+waveForm = 2;           % Used for the stimulusBraasch function
+% integer to select a waveform:
+%           0 - Sine Wave
+%           1 - Triangle Wave
+%           2 - Bandpass Noise
+%           3 - White Noise (Uniform Distribution)
+%           4 - Two sine waves (1st & 3rd harmonics; f = f of 2nd harmonic)
+%           5 - Three sine waves (1st, 2nd & 3rd harmonics; f = f of 2nd harmonic)
+%           6 - peak train
 
-% Create a data object based on parts of the right ear signal
+length = 400;           % Signal length in ms
+fc = 500;               % For periodic waves: Frequency in Hz,
+                        % For Bandpass Noise: Fc of the bandpass filter
+bw = 800;               % Bandwidth of the FFT bandpass filters
+itd = 0.6;              % ITD in ms (applied in positive/negative pair to 
+                        % stimulusBraasch function)                      
+ISI = 3;                % Inter-Stimulus Interval in ms
+attackTime = 20;        % Attach time in ms
+decayTime = 20;         % Decay time in ms
+operationMode = 0;      % Used for the stimulusBraasch function
+%           0: specified operation (both lead and lag are present), 
+%           1: switch off the lead, 2: switch off the lag
+
+lagLevel = 0;           % Lag level in dB
+
+% Binaural test stimulus using Braasch's function
+% Usage: 
+%   stimulusBraasch(Fs, mode, len, f, bw, itd1, itd2, isi, at, dc, nn, lag_level)
+% This function is copied from Braasch's model into AFE src/Tools folder.
+% Parameters:
+% 
+%         Fs:    sampling frequency
+%         mode:  integer to select a waveform
+%         len:   Signal length in milliseconds
+%         f:     For periodic waves: Frequency in Hz
+%                For Bandpass Noise: Fc of the bandpass filter
+%         bw:    Bandwidth of the FFT bandpass filters
+%         itd1,
+%         itd2:  ITDs in milliseconds
+%               (positive for right channel first - left channel last,
+%               negative for left channel first - right channel last)
+%               Indices 1 and 2 correspond to the incidence of the
+%               the two signals.
+%         isi:   ISI in milliseconds
+%         at:    attack time (ms)
+%         dc:    decay time (ms)
+%         nn:    0: specified operation, 1: switch off S1, 2: switch off S2
+%         lag_level: lag level in dB
+
+x = stimulusBraasch(fsHz, waveForm, length, fc, bw, ...
+    itd, -itd, ...
+    ISI, attackTime, decayTime, ...
+    operationMode, ...
+    db2amp(lagLevel));
+
+% Create a data object
 dObj = dataObject(x, fsHz);
 
 
@@ -25,10 +73,10 @@ requests = 'precedence';
 fb_lowFreqHz = 100;
 fb_highFreqHz = 1400;
 
-% Note from Braasch:
+% Note copied from Braasch's code:
 % minimum windowlength needs to be in order of binaural sluggishness for
-% the model to operate properly (now set to 1024 tabs). Needs sufficient
-% length to perform autocorrelation over both lead and lag.
+% the model to operate properly. Needs sufficient length to perform 
+% autocorrelation over both lead and lag.
 prec_wSizeSec = 0.02;
 prec_hSizeSec = 0.01;
 
